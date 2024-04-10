d43b7a843b8d14132782187306a855682de0d085
Ankit Srivastava <99680514+Bytecode-Magnum@users.noreply.github.com>
2024-04-11 02:24:17 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from transformers import pipeline
-from tqdm import tqdm
-
-
-df1=pd.read_csv(r'C:\Users\ankit\Desktop\HotelList\artifacts\data\shojareviews.csv')
-df2=pd.read_csv(r'C:\Users\ankit\Desktop\HotelList\artifacts\data\CleanedShojaHotel.csv')
-print(df1.dtypes)
-print(df2.dtypes)
